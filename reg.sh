89e57d803d44f55b5d858cceed62c54a602b6b51
manojsid <siddammanoj1998@gmail.com>
2020-06-16 17:02:54 +0530

    Username Pattren added

--- reg.sh
+++ reg.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "Welcome to User Registration"
+shopt -s extglob
+
+read -p "Enter the Valid username: " $username
+
+if [[ $username =~ ^([A-Z]{1})+([a-z]{2,})$ ]]; then
+	
+	echo "valid- " $username
+else
+	echo "Invalid- " $username 
+
+fi
