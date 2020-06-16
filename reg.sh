62eac9e5ab5db1def5c99cfa897fbcc706dfec2e
manojsid <siddammanoj1998@gmail.com>
2020-06-16 17:13:17 +0530

    Last name pattren added

--- reg.sh
+++ reg.sh
@@ -2,12 +2,14 @@
 echo "Welcome to User Registration"
 shopt -s extglob
 
-read -p "Enter the Valid username: " $username
-
-if [[ $username =~ ^([A-Z]{1})+([a-z]{2,})$ ]]; then
+read -p "Enter the Valid firstname: " $firstname
+read -p "Eter the valid Last name: " $lastname
+pat="^([A-Z]{1})+([a-z]{2,})$"
+if [[ $firstname =~ $pat && $lastname =~ $pat ]]; then
 	
-	echo "valid- " $username
+	echo "valid-  $firstname $lastname"
 else
-	echo "Invalid- " $username 
+	echo "Invalid-  $firstname $lastname"
 
 fi
+
