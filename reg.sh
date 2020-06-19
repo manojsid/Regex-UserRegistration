b4af1cb044f6739491b69d10aeca0f1042df0ec4
manojsid <siddammanoj1998@gmail.com>
2020-06-19 09:15:32 +0530

    Password matching added(Min 8 charcters req)

--- reg.sh
+++ reg.sh
@@ -10,22 +10,32 @@ pat="^([A-Z]{1})+([a-z]{2,})$"
 pattren="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$"
 phone_pattren="^[0-9]{2}[[:space:]][0-9]{10}$"
 if [[ $firstname =~ $pat && $lastname =~ $pat ]]; then
-	
 	echo "valid-  $firstname $lastname"
-	if [[ $email =~ $Email_pattren ]]; then
-		echo "Your email is sucessfully valid"
-	else
-	echo "Please provide valid Email ID"	
-	fi
-
-	if [[ $Mobile =~ $phone_pattren ]]; then
-		#statements
-		echo "Your Mobile number is verified"
+		
+		if [[ $email =~ $Email_pattren ]]; then
+			echo "Your email is sucessfully valid"
 		else
-			echo "Please provide the Valid Mobile number"
-	fi
+			echo "Please provide valid Email ID"	
+		fi
+			
+			if [[ $Mobile =~ $phone_pattren ]]; then
+				echo "Your Mobile number is verified"
+			else
+				echo "Please provide the Valid Mobile number"
+			fi
+
 else
 	echo "Invalid-  $firstname $lastname"
 
 fi
 
+read -p "Enter your Password: " pass
+function password_check(){
+	passpattern1="^([a-zA-Z0-9!@#]{8,})$"
+	if [[ $pass =~ $passpattern1 ]]; then
+		echo "Password is Valid."
+	else
+		echo "Password is Invalid. Please try again."
+	fi
+}
+password_check
